21fb3d1461cd232ec75a3947a21f15a72259f953
jf2028mp <jeffry5800@gmail.com>
2019-01-29 17:52:08 -0600

    Updates

---
+++ precache-manifest.59801e4f9e634ec5efef59011d452251.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b43a63a2932c900937f",
+    "url": "/static/js/main.3b43a63a.chunk.js"
+  },
+  {
+    "revision": "805894136d7a82f2b355",
+    "url": "/static/js/1.80589413.chunk.js"
+  },
+  {
+    "revision": "3b43a63a2932c900937f",
+    "url": "/static/css/main.5fa94f36.chunk.css"
+  },
+  {
+    "revision": "fe04994345e046cf2f3f223f54a7e350",
+    "url": "/index.html"
+  }
+];
