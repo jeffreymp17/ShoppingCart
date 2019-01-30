76ceffff5d4157cfd6e8ce981c60e3009e97e7e6
jf2028mp <jeffry5800@gmail.com>
2019-01-30 13:46:49 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ShoppingCart/precache-manifest.85b8f02f0a35695cc9055fecdfa8c6c9.js"
+  "/ShoppingCart/precache-manifest.12c465d06ae870f8772ddfe83719ae5a.js"
 );
 
 workbox.clientsClaim();
