642bc604be7155bd470e44d210c6df071f208837
jf2028mp <jeffry5800@gmail.com>
2019-01-30 14:39:23 -0600

    Updates

---
+++ precache-manifest.12c465d06ae870f8772ddfe83719ae5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a52ac06f1af0469c24",
+    "url": "/ShoppingCart/static/js/runtime~main.f1a52ac0.js"
+  },
+  {
+    "revision": "606789218571c2c5fd8d",
+    "url": "/ShoppingCart/static/js/main.60678921.chunk.js"
+  },
+  {
+    "revision": "e871e346b7bf37e14338",
+    "url": "/ShoppingCart/static/js/1.e871e346.chunk.js"
+  },
+  {
+    "revision": "606789218571c2c5fd8d",
+    "url": "/ShoppingCart/static/css/main.4314bde1.chunk.css"
+  },
+  {
+    "revision": "99a93cdb350fac622703524bea3ac7a8",
+    "url": "/ShoppingCart/index.html"
+  }
+];
